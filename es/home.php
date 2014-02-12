873fb7c2276e205f68b623bf5f78a8c4b82698a2
Miguel Angel Melon Perez <miguelfito@gmail.com>
2014-02-12 14:09:00 +0100

    Workspace fix
    - Fixed some issues on workspace div. So many others pending...

--- es/home.php
+++ es/home.php
@@ -2,14 +2,20 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Using the same favicon from perspectiva-alemania.com site -->
+	<link rel="shortcut icon" href="http://www.perspectiva-alemania.com/wp-content/themes/perspectiva2013/bilder/favicon.png">
+	<!-- Using the favicon for touch-devices shortcut -->
+	<link rel="apple-touch-icon" href="../common/img/apple-touch-icon.png">
+
 	<title>Inicio</title>
 
 	<!-- Custom styles for this template -->
 	<link href="../common/css/styles.css" rel="stylesheet">
 	<link href="../common/css/design.css" rel="stylesheet">
 
-	<script src="../common/js/functions.js" type="text/javascript"></script>
-	<!-- <script type="text/javascript" src="../js/jquery-1.10.1.min.js"></script> ONLY IF USED jQuery -->
 </head>
 
 <body>
@@ -77,125 +83,117 @@
 		</div>	
 
 
-		<?php 
+		 
+		<!-- /* En $myFile guardo el nombre del fichero php que WC está tratando en ese instante. Necesario para mostrar
+		* el resto de menús de nivel 1 cuando navegue por ellos, y saber cuál es el activo (id='onlink')
+		*/ -->
+		<?php
+		$myFile = 'home';
+		$userRow = getDBrow('users', 'login', $_SESSION['loglogin']);
+		?>
 
-/* En $myFile guardo el nombre del fichero php que WC está tratando en ese instante. Necesario para mostrar
-* el resto de menús de nivel 1 cuando navegue por ellos, y saber cuál es el activo (id='onlink')
-*/
-$myFile = 'home';
-$userRow = getDBrow('users', 'login', $_SESSION['loglogin']);
-?>
-<div id="mainmenu">
-	<ul class="navbar1">
-		<?php 
-/*
-if(($userRow['profile'] == 'Administrador') || ($userRow['profile'] == 'SuperAdmin')){
-echo "<li><a href='home.php' id='onlink'>Inicio</a></li>";
-echo "<li><a href='administration.php'>Administrar</a></li>";
-}
-elseif($userRow['profile'] == 'Lector'){
-echo "<li><a href='home.php' id='onlink'>Inicio</a></li>";
-}
 
-else{
-echo "<li><a href='home.php' id='onlink'>Inicio</a></li>";
-}
-*/
-$mainKeysRow = getDBcompletecolumnID('key', 'mainNames', 'id');
-$mainNamesRow = getDBcompletecolumnID('esName', 'mainNames', 'id');
-$j = 0;
-foreach($mainKeysRow as $i){
-	if(getDBsinglefield('active', $i, 'profile', $userRow['profile'])){
-		if($myFile == $i){
-			echo "<li><a href=$i.php id='onlink'>" . utf8_encode($mainNamesRow[$j]) . "</a></li>";
-			$j++;
-		}
-		else{
-			echo "<li><a href=$i.php>" . utf8_encode($mainNamesRow[$j]) . "</a></li>";
-			$j++;
-		}
-	}
-}
-?>
-</ul>
-</div>
+
 
 <div class="workspace">
-	<div class="leftbox">
-		<div class="css-treeview">
-			<ul>
-				<?php
-				$namesTable = $myFile.'Names';
-				$numCols = getDBnumcolumns($myFile);
-				$myFileProfileRow = getDBrow($myFile, 'profile', $userRow['profile']);
-				for($j=3;$j<$numCols;$j++){
-					$colNamej = getDBcolumnname($myFile, $j);
-					if(($myFileProfileRow[$j] == 1) && ($subLevelMenu = getDBsinglefield2('esName', $namesTable, 'key', $colNamej, 'level', '2'))){
-						if(!getDBsinglefield2('esName', $namesTable, 'fatherKey', $colNamej, 'level', '3')){
-							$level2File = getDBsinglefield('key', $namesTable, 'esName', $subLevelMenu);
-//echo "<li><a href=home/$level2File.php>" . $subLevelMenu . "</a></li>";
-							echo "<li><a href=home/$level2File.php>" . utf8_encode($subLevelMenu) . "</a></li>";
+
+		<div id="navbar" role="navigation">
+			<ul class="nav nav-tabs">
+				<?php 
+				$mainKeysRow = getDBcompletecolumnID('key', 'mainNames', 'id');
+				$mainNamesRow = getDBcompletecolumnID('esName', 'mainNames', 'id');
+				$j = 0;
+				foreach($mainKeysRow as $i){
+					if(getDBsinglefield('active', $i, 'profile', $userRow['profile'])){
+						if($myFile == $i){
+							echo "<li class='active'><a href=$i.php>" . $mainNamesRow[$j] . "</a></li>";
+							$j++;
 						}
 						else{
-							$arrayKeys = array();
-							$arrayKeys = getDBcolumnvalue('key', $namesTable, 'fatherKey', $colNamej);
-							$checkFinished = 0;
-							$l = 1;
-							foreach($arrayKeys as $k){
-								if($checkFinished == 0){
-									if(($myFileProfileRow[$j+$l] == 1) && (getDBsinglefield($k, $myFile, 'profile', $userRow['profile']))){
-										$level3File = $k;
-										$checkFinished = 1;
-									}
-									else{
-										$l++;
-									}
-								}
-							}
-//echo "<li><a href=home/$level3File.php>" . $subLevelMenu . "</a></li>";
-							echo "<li><a href=home/$level3File.php>" . utf8_encode($subLevelMenu) . "</a></li>";
+							echo "<li><a href=$i.php>" . $mainNamesRow[$j] . "</a></li>";
+							$j++;
 						}
 					}
 				}
 				?>
 			</ul>
 		</div>
-	</div> <!-- "leftbox" -->
 
-	<div class="rightbox">
 
-		<!-- SI ES LA PRIMERA VEZ QUE SE CONECTA UN CANDIDATO, O LE VUELVEN A DAR ACCESO PARA RELLENAR OTRO CV, LE MANDAMOS DIRECTAMENTE AL FORMULARIO -->
+		<div class="panel panel-default" id="mainContent">
+			<div class="panel-body" id="mainContent">
+				<div class="leftbox">
+					<div class="css-treeview">
+						<ul>
+							<?php
+							$namesTable = $myFile.'Names';
+							$numCols = getDBnumcolumns($myFile);
+							$myFileProfileRow = getDBrow($myFile, 'profile', $userRow['profile']);
+							for($j=3;$j<$numCols;$j++){
+								$colNamej = getDBcolumnname($myFile, $j);
+								if(($myFileProfileRow[$j] == 1) && ($subLevelMenu = getDBsinglefield2('esName', $namesTable, 'key', $colNamej, 'level', '2'))){
+									if(!getDBsinglefield2('esName', $namesTable, 'fatherKey', $colNamej, 'level', '3')){
+										$level2File = getDBsinglefield('key', $namesTable, 'esName', $subLevelMenu);
+										echo "<li><a href=home/$level2File.php>" . utf8_encode($subLevelMenu) . "</a></li>";
+									}
+									else{
+										$arrayKeys = array();
+										$arrayKeys = getDBcolumnvalue('key', $namesTable, 'fatherKey', $colNamej);
+										$checkFinished = 0;
+										$l = 1;
+										foreach($arrayKeys as $k){
+											if($checkFinished == 0){
+												if(($myFileProfileRow[$j+$l] == 1) && (getDBsinglefield($k, $myFile, 'profile', $userRow['profile']))){
+													$level3File = $k;
+													$checkFinished = 1;
+												}
+												else{
+													$l++;
+												}
+											}
+										}
+										echo "<li><a href=home/$level3File.php>" . utf8_encode($subLevelMenu) . "</a></li>";
+									}
+								}
+							}
+							?>
+						</ul>
+					</div>
+				</div> <!-- "leftbox" -->
 
-		<?php 
-		echo "<p><span id='leftmsg'>Noticias</span></p><hr>";
-//if($userRow['profile'] == 'Administrador'){
-		if(($userRow['profile'] == 'Administrador') || ($userRow['profile'] == 'SuperAdmin')){
-//echo 'Noticias:<br>';
-//echo 'Existen';
-			if((getDBrowsnumber('cVitaes') == 0) || ($numPendingCVs = count($cvIDs = getDBcolumnvalue('id', 'cVitaes', 'cvStatus', 'pending')))){
-				echo "No existen CVs por clasificar.";
-			}
-			else{
-				echo "Existen <a href=./home/pendingCVs.php>" . $numPendingCVs . " </a> CVs por clasificar.";
-			}
-		}
-		elseif($userRow['profile'] == 'Lector'){
-			echo "-- DEFINIR SE SE QUIERE O NO QUE UN PERFIL \"Lector\" PUEDA REVISAR CVs --";
-			if((getDBrowsnumber('cVitaes') == 0) || ($numPendingCVs = count($cvIDs = getDBcolumnvalue('id', 'cVitaes', 'cvStatus', 'pending')))){
-				echo "No existen CVs por clasificar.";
-			}
-			else{
-				echo "Existen <a href=./home/pendingCVs.php>" . $numPendingCVs . " </a> CVs por clasificar.";
-			}
-		}
-		else{
-			echo "-- SE ENTIENDE QUE SI UN \"\" TIENE ACCESO A LA ZONA PRIVADA DE LA PAGINA ES PORQUE SE LE HA CONCEDIDO PARA RELLENAR OTRO CV --";
-			include 'blankform.php';
+				<div class="rightbox">
 
-		}
-		?>
+					<!-- SI ES LA PRIMERA VEZ QUE SE CONECTA UN CANDIDATO, O LE VUELVEN A DAR ACCESO PARA RELLENAR OTRO CV, LE MANDAMOS DIRECTAMENTE AL FORMULARIO -->
 
-	</div>
+					<?php 
+					echo "<p><span id='leftmsg'>Noticias</span></p><hr>";
+
+					if(($userRow['profile'] == 'Administrador') || ($userRow['profile'] == 'SuperAdmin')){
+						if((getDBrowsnumber('cVitaes') == 0) || ($numPendingCVs = count($cvIDs = getDBcolumnvalue('id', 'cVitaes', 'cvStatus', 'pending')))){
+							echo "No existen CVs por clasificar.";
+						}
+						else{
+							echo "Existen <a href=./home/pendingCVs.php>" . $numPendingCVs . " </a> CVs por clasificar.";
+						}
+					}
+					elseif($userRow['profile'] == 'Lector'){
+						echo "-- DEFINIR SE SE QUIERE O NO QUE UN PERFIL \"Lector\" PUEDA REVISAR CVs --";
+						if((getDBrowsnumber('cVitaes') == 0) || ($numPendingCVs = count($cvIDs = getDBcolumnvalue('id', 'cVitaes', 'cvStatus', 'pending')))){
+							echo "No existen CVs por clasificar.";
+						}
+						else{
+							echo "Existen <a href=./home/pendingCVs.php>" . $numPendingCVs . " </a> CVs por clasificar.";
+						}
+					}
+					else{
+						echo "-- SE ENTIENDE QUE SI UN \"\" TIENE ACCESO A LA ZONA PRIVADA DE LA PAGINA ES PORQUE SE LE HA CONCEDIDO PARA RELLENAR OTRO CV --";
+						include 'blankform.php';
+
+					}
+					?>
+				</div> <!-- "rightbox" -->
+			</div> <!-- "panel-body" -->
+		</div>
 </div>
 <?php
 
@@ -221,9 +219,7 @@ foreach($mainKeysRow as $i){
 
 	<!-- Site own functions -->
 	<script src="../common/js/functions.js"></script>
-
-	<!-- Do you wanna use tooltips or validation instead of? -->
-	<!-- <script src="../common/js/application.js"></script> -->
+	<script src="../common/js/application.js"></script>
 
 </body>
 </html>
