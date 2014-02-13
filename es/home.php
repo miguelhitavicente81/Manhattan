df796fc36f64387b5e3a8a7ae1e95118f1cec92a
Miguel Angel Melon Perez <miguelfito@gmail.com>
2014-02-13 20:25:20 +0100

    Top bar completed
    - Fixed top bar finally completed. Now it’s responsive and much more.
    - Body behaviour for top padding changed to correct media screen
    response.
    
    TO-DO:
    - Code clean

--- es/home.php
+++ es/home.php
@@ -12,6 +12,7 @@
 	<!-- Custom styles for this template -->
 	<link href="../common/css/styles.css" rel="stylesheet">
 	<link href="../common/css/design.css" rel="stylesheet">
+	<link href="../common/css/docs.css" rel="stylesheet">
 
 	<!-- Using the same favicon from perspectiva-alemania.com site -->
 	<link rel="shortcut icon" href="http://www.perspectiva-alemania.com/wp-content/themes/perspectiva2013/bilder/favicon.png">
@@ -52,24 +53,22 @@
 
 
 		<!-- Static navbar -->
-		<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="navbar navbar-default navbar-fixed-top" role="navigation" id="fixed-top-bar">
 			<div class="top-page-color"></div>
 			<div class="container-fluid">
 				<div class="navbar-header">
 					<a href="http://www.perspectiva-alemania.com/" title="Perspectiva Alemania">
 						<img src="../common/img/logo.png" alt="Perspectiva Alemania">
 					</a>
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
 				</div>
-				<div class="navbar-collapse collapse">
-					<ul class="nav navbar-nav navbar-right">
+				<!-- <div class="navbar-collapse collapse"> -->
+					<div class="nav navbar-nav navbar-right">
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Menú <b class="caret"></b></a>
+							<button type="button" class="navbar-toggle always-visible" data-toggle="dropdown">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
 							<ul class="dropdown-menu">
 								<li class="dropdown-header">Conectado como: <?php echo $_SESSION['loglogin']; ?></li>
 								<li class="divider"></li>
@@ -80,10 +79,10 @@
 								<li><a href="endsession.php">Salir</a></li>
 							</ul>
 						</li>
-					</ul>
-				</div><!--/.nav-collapse -->
+					</div>
+				<!-- </div><!--/.nav-collapse -->
 			</div><!--/.container-fluid -->
-		</div>	
+		</div>	<!--/Static navbar -->
 
 
 		 
@@ -97,242 +96,221 @@
 
 
 
-
-
-	<div class="workspace">
-
-		<!-- Tabbed navbar inside Workspace -->
-		<div id="navbar" role="navigation">
-			<ul class="nav nav-tabs">
-				<?php 
-				$mainKeysRow = getDBcompletecolumnID('key', 'mainNames', 'id');
-				$mainNamesRow = getDBcompletecolumnID('esName', 'mainNames', 'id');
-				$j = 0;
-				foreach($mainKeysRow as $i){
-					if(getDBsinglefield('active', $i, 'profile', $userRow['profile'])){
-						if($myFile == $i){
-							echo "<li class='active'><a href=$i.php>" . $mainNamesRow[$j] . "</a></li>";
-							$j++;
-						}
-						else{
-							echo "<li><a href=$i.php>" . $mainNamesRow[$j] . "</a></li>";
-							$j++;
-						}
-					}
-				}
-				?>
-			</ul>
-		</div> <!-- Tabbed navbar --> 
-
-		<!-- Main view inside Workspace -->
-		<div class="panel panel-default transparent">
-
-			<!-- Panel inside Workspace -->
-			<div class="panel-body sidebar-parent scrollable no-padding"> 
-
-				
-				<div class="col-sm-3 col-md-2 sidebar transparent">
-					<ul class="nav nav-sidebar">
-						<?php
-							$namesTable = $myFile.'Names';
-							$numCols = getDBnumcolumns($myFile);
-							$myFileProfileRow = getDBrow($myFile, 'profile', $userRow['profile']);
-							for($j=3;$j<$numCols;$j++) {
-								$colNamej = getDBcolumnname($myFile, $j);
-								if(($myFileProfileRow[$j] == 1) && ($subLevelMenu = getDBsinglefield2('esName', $namesTable, 'key', $colNamej, 'level', '2'))) {
-									if(!getDBsinglefield2('esName', $namesTable, 'fatherKey', $colNamej, 'level', '3')){
-										$level2File = getDBsinglefield('key', $namesTable, 'esName', $subLevelMenu);
-										echo "<li><a href=home/$level2File.php>" . $subLevelMenu . "</a></li>";
-									}
-									else{
-										$arrayKeys = array();
-										$arrayKeys = getDBcolumnvalue('key', $namesTable, 'fatherKey', $colNamej);
-										$checkFinished = 0;
-										$l = 1;
-										foreach($arrayKeys as $k){
-											if($checkFinished == 0){
-												if(($myFileProfileRow[$j+$l] == 1) && (getDBsinglefield($k, $myFile, 'profile', $userRow['profile']))){
-													$level3File = $k;
-													$checkFinished = 1;
-												}
-												else{
-													$l++;
+		<div class="container bs-docs-container">
+			<div class="row">
+				<div class="col-md-3">
+						<div class="bs-sidebar hidden-print affix-top" role="complementary">
+							<ul class="nav bs-sidenav">
+								<li class="active">
+									<a href="home.php">Inicio</a>
+									<ul class="nav">
+										<?php
+											$namesTable = $myFile.'Names';
+											$numCols = getDBnumcolumns($myFile);
+											$myFileProfileRow = getDBrow($myFile, 'profile', $userRow['profile']);
+											for($j=3;$j<$numCols;$j++) {
+												$colNamej = getDBcolumnname($myFile, $j);
+												if(($myFileProfileRow[$j] == 1) && ($subLevelMenu = getDBsinglefield2('esName', $namesTable, 'key', $colNamej, 'level', '2'))) {
+													if(!getDBsinglefield2('esName', $namesTable, 'fatherKey', $colNamej, 'level', '3')){
+														$level2File = getDBsinglefield('key', $namesTable, 'esName', $subLevelMenu);
+														echo "<li><a href=home/$level2File.php>" . $subLevelMenu . "</a></li>";
+													}
+													else{
+														$arrayKeys = array();
+														$arrayKeys = getDBcolumnvalue('key', $namesTable, 'fatherKey', $colNamej);
+														$checkFinished = 0;
+														$l = 1;
+														foreach($arrayKeys as $k){
+															if($checkFinished == 0){
+																if(($myFileProfileRow[$j+$l] == 1) && (getDBsinglefield($k, $myFile, 'profile', $userRow['profile']))){
+																	$level3File = $k;
+																	$checkFinished = 1;
+																}
+																else{
+																	$l++;
+																}
+															}
+														}
+														echo "<li><a href=home/$level3File.php>" . $subLevelMenu . "</a></li>";
+													}
 												}
 											}
-										}
-										echo "<li><a href=home/$level3File.php>" . $subLevelMenu . "</a></li>";
-									}
+										?>
+									</ul> <!-- class="nav" -->
+								</li> <!-- class="active" -->
+							</ul> <!-- class="nav bs-sidenav" -->
+						</div> <!-- class="bs-sidebar hidden-print affix-top" role="complementary" -->
+				</div> <!-- col-md-3 -->
+
+				<div class="col-md-9 scrollable" role="main">
+					<div class="bs-docs-section">
+
+						<h1 class="page-header">Noticias</span></h1>
+
+						<p class="lead">
+						<?php 
+
+							if(($userRow['profile'] == 'Administrador') || ($userRow['profile'] == 'SuperAdmin')){
+								if((getDBrowsnumber('cVitaes') == 0) || ($numPendingCVs = count($cvIDs = getDBcolumnvalue('id', 'cVitaes', 'cvStatus', 'pending')))){
+									echo "No existen CVs por clasificar.";
+								}
+								else{
+									echo "Existen <a href=./home/pendingCVs.php>" . $numPendingCVs . " </a> CVs por clasificar.";
 								}
 							}
+							elseif($userRow['profile'] == 'Lector'){
+								echo "-- DEFINIR SE SE QUIERE O NO QUE UN PERFIL \"Lector\" PUEDA REVISAR CVs --";
+								if((getDBrowsnumber('cVitaes') == 0) || ($numPendingCVs = count($cvIDs = getDBcolumnvalue('id', 'cVitaes', 'cvStatus', 'pending')))){
+									echo "No existen CVs por clasificar.";
+								}
+								else{
+									echo "Existen <a href=./home/pendingCVs.php>" . $numPendingCVs . " </a> CVs por clasificar.";
+								}
+							}
+							else{
+								echo "-- SE ENTIENDE QUE SI UN \"\" TIENE ACCESO A LA ZONA PRIVADA DE LA PAGINA ES PORQUE SE LE HA CONCEDIDO PARA RELLENAR OTRO CV --";
+								include 'blankform.php';
+
+							}
 						?>
-					</ul>
-				</div> <!-- col-sm-3 col-md-2 sidebar -->
-				
+						</p>
 
-				<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+						<h2 class="sub-header">Section title</h2>
 
-					<h1 class="page-header">Noticias</span></h1>
+						<div class="table-responsive">
+							<table class="table table-striped">
+								<thead>
+									<tr>
+										<th>#</th>
+										<th>Header</th>
+										<th>Header</th>
+										<th>Header</th>
+										<th>Header</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>1,001</td>
+										<td>Lorem</td>
+										<td>ipsum</td>
+										<td>dolor</td>
+										<td>sit</td>
+									</tr>
+									<tr>
+										<td>1,002</td>
+										<td>amet</td>
+										<td>consectetur</td>
+										<td>adipiscing</td>
+										<td>elit</td>
+									</tr>
+									<tr>
+										<td>1,003</td>
+										<td>Integer</td>
+										<td>nec</td>
+										<td>odio</td>
+										<td>Praesent</td>
+									</tr>
+									<tr>
+										<td>1,003</td>
+										<td>libero</td>
+										<td>Sed</td>
+										<td>cursus</td>
+										<td>ante</td>
+									</tr>
+									<tr>
+										<td>1,004</td>
+										<td>dapibus</td>
+										<td>diam</td>
+										<td>Sed</td>
+										<td>nisi</td>
+									</tr>
+									<tr>
+										<td>1,005</td>
+										<td>Nulla</td>
+										<td>quis</td>
+										<td>sem</td>
+										<td>at</td>
+									</tr>
+									<tr>
+										<td>1,006</td>
+										<td>nibh</td>
+										<td>elementum</td>
+										<td>imperdiet</td>
+										<td>Duis</td>
+									</tr>
+									<tr>
+										<td>1,007</td>
+										<td>sagittis</td>
+										<td>ipsum</td>
+										<td>Praesent</td>
+										<td>mauris</td>
+									</tr>
+									<tr>
+										<td>1,008</td>
+										<td>Fusce</td>
+										<td>nec</td>
+										<td>tellus</td>
+										<td>sed</td>
+									</tr>
+									<tr>
+										<td>1,009</td>
+										<td>augue</td>
+										<td>semper</td>
+										<td>porta</td>
+										<td>Mauris</td>
+									</tr>
+									<tr>
+										<td>1,010</td>
+										<td>massa</td>
+										<td>Vestibulum</td>
+										<td>lacinia</td>
+										<td>arcu</td>
+									</tr>
+									<tr>
+										<td>1,011</td>
+										<td>eget</td>
+										<td>nulla</td>
+										<td>Class</td>
+										<td>aptent</td>
+									</tr>
+									<tr>
+										<td>1,012</td>
+										<td>taciti</td>
+										<td>sociosqu</td>
+										<td>ad</td>
+										<td>litora</td>
+									</tr>
+									<tr>
+										<td>1,013</td>
+										<td>torquent</td>
+										<td>per</td>
+										<td>conubia</td>
+										<td>nostra</td>
+									</tr>
+									<tr>
+										<td>1,014</td>
+										<td>per</td>
+										<td>inceptos</td>
+										<td>himenaeos</td>
+										<td>Curabitur</td>
+									</tr>
+									<tr>
+										<td>1,015</td>
+										<td>sodales</td>
+										<td>ligula</td>
+										<td>in</td>
+										<td>libero</td>
+									</tr>
+								</tbody>
+							</table>
+						</div> <!-- table-responsive -->
+					</div> <!-- bs-docs-section -->
+				</div> <!-- col-md-9 scrollable role=main -->
+			</div> <!-- row -->
+		</div> <!-- class="container bs-docs-container" -->
 
-					<?php 
 
-						if(($userRow['profile'] == 'Administrador') || ($userRow['profile'] == 'SuperAdmin')){
-							if((getDBrowsnumber('cVitaes') == 0) || ($numPendingCVs = count($cvIDs = getDBcolumnvalue('id', 'cVitaes', 'cvStatus', 'pending')))){
-								echo "No existen CVs por clasificar.";
-							}
-							else{
-								echo "Existen <a href=./home/pendingCVs.php>" . $numPendingCVs . " </a> CVs por clasificar.";
-							}
-						}
-						elseif($userRow['profile'] == 'Lector'){
-							echo "-- DEFINIR SE SE QUIERE O NO QUE UN PERFIL \"Lector\" PUEDA REVISAR CVs --";
-							if((getDBrowsnumber('cVitaes') == 0) || ($numPendingCVs = count($cvIDs = getDBcolumnvalue('id', 'cVitaes', 'cvStatus', 'pending')))){
-								echo "No existen CVs por clasificar.";
-							}
-							else{
-								echo "Existen <a href=./home/pendingCVs.php>" . $numPendingCVs . " </a> CVs por clasificar.";
-							}
-						}
-						else{
-							echo "-- SE ENTIENDE QUE SI UN \"\" TIENE ACCESO A LA ZONA PRIVADA DE LA PAGINA ES PORQUE SE LE HA CONCEDIDO PARA RELLENAR OTRO CV --";
-							include 'blankform.php';
 
-						}
-					?>
-
-					<h2 class="sub-header">Section title</h2>
-
-					<div class="table-responsive">
-						<table class="table table-striped">
-							<thead>
-								<tr>
-									<th>#</th>
-									<th>Header</th>
-									<th>Header</th>
-									<th>Header</th>
-									<th>Header</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>1,001</td>
-									<td>Lorem</td>
-									<td>ipsum</td>
-									<td>dolor</td>
-									<td>sit</td>
-								</tr>
-								<tr>
-									<td>1,002</td>
-									<td>amet</td>
-									<td>consectetur</td>
-									<td>adipiscing</td>
-									<td>elit</td>
-								</tr>
-								<tr>
-									<td>1,003</td>
-									<td>Integer</td>
-									<td>nec</td>
-									<td>odio</td>
-									<td>Praesent</td>
-								</tr>
-								<tr>
-									<td>1,003</td>
-									<td>libero</td>
-									<td>Sed</td>
-									<td>cursus</td>
-									<td>ante</td>
-								</tr>
-								<tr>
-									<td>1,004</td>
-									<td>dapibus</td>
-									<td>diam</td>
-									<td>Sed</td>
-									<td>nisi</td>
-								</tr>
-								<tr>
-									<td>1,005</td>
-									<td>Nulla</td>
-									<td>quis</td>
-									<td>sem</td>
-									<td>at</td>
-								</tr>
-								<tr>
-									<td>1,006</td>
-									<td>nibh</td>
-									<td>elementum</td>
-									<td>imperdiet</td>
-									<td>Duis</td>
-								</tr>
-								<tr>
-									<td>1,007</td>
-									<td>sagittis</td>
-									<td>ipsum</td>
-									<td>Praesent</td>
-									<td>mauris</td>
-								</tr>
-								<tr>
-									<td>1,008</td>
-									<td>Fusce</td>
-									<td>nec</td>
-									<td>tellus</td>
-									<td>sed</td>
-								</tr>
-								<tr>
-									<td>1,009</td>
-									<td>augue</td>
-									<td>semper</td>
-									<td>porta</td>
-									<td>Mauris</td>
-								</tr>
-								<tr>
-									<td>1,010</td>
-									<td>massa</td>
-									<td>Vestibulum</td>
-									<td>lacinia</td>
-									<td>arcu</td>
-								</tr>
-								<tr>
-									<td>1,011</td>
-									<td>eget</td>
-									<td>nulla</td>
-									<td>Class</td>
-									<td>aptent</td>
-								</tr>
-								<tr>
-									<td>1,012</td>
-									<td>taciti</td>
-									<td>sociosqu</td>
-									<td>ad</td>
-									<td>litora</td>
-								</tr>
-								<tr>
-									<td>1,013</td>
-									<td>torquent</td>
-									<td>per</td>
-									<td>conubia</td>
-									<td>nostra</td>
-								</tr>
-								<tr>
-									<td>1,014</td>
-									<td>per</td>
-									<td>inceptos</td>
-									<td>himenaeos</td>
-									<td>Curabitur</td>
-								</tr>
-								<tr>
-									<td>1,015</td>
-									<td>sodales</td>
-									<td>ligula</td>
-									<td>in</td>
-									<td>libero</td>
-								</tr>
-							</tbody>
-						</table>
-					</div> <!-- table-responsive -->
-				</div> <!-- col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main -->
-			
-			</div> <!-- panel-body --> 
-		</div> <!-- panel -->
-	</div> <!-- workspace -->
 
 	<?php
 
@@ -343,7 +321,7 @@
 
 <!-- Footer bar & info
 	================================================== -->
-	<div id="footer">
+	<div class="hidden-xs hidden-sm" id="footer">
 		<div class="container">
 			<p class="text-muted">&copy; Perspectiva Alemania, S.L.</p>
 		</div>
@@ -359,6 +337,7 @@
 	<!-- Site own functions -->
 	<script src="../common/js/functions.js"></script>
 	<script src="../common/js/application.js"></script>
+	<script src="../common/js/docs.min.js"></script>
 
 </body>
 </html>
