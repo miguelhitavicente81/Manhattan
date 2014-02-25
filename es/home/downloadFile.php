8e4d94665b93a945f112a43dffe9ed845b7f8eb3
miguelhita81 <miguel.hita.vicente@gmail.com>
2014-02-25 19:57:57 +0100

    Modificaciones 25/02/2014
    En searchResult.php:
    -los resultados de Select se tienen en etiquetas en vez de numeros
    -La tabla de resultados es configurable
    -Eliminacion del problema de la generiacion de pdfs dado que la copia de
    archivos , compresion y descarga la hace el propio php siendo
    multiplataforma
    
    En downloadFile:
    
    Correcion de rutas ( poniendo relativas) y modificacion del nombre de
    zip con fecha

--- es/home/downloadFile.php
+++ es/home/downloadFile.php
@@ -1,10 +1,10 @@
 <?php
 	$filea=$_GET["doc"];
-	$file = "../../cvs/$filea";
-	echo "Fichero montado: " . $file;
+	$file = "$filea";
+	$desc=date("YmdHis");
 	header ("Content-Type: application/octet-stream");
 	header ("Accept-Ranges: bytes");
 	header ("Content-Length: ".filesize($file));
-	header ("Content-Disposition: attachment; filename=".$filea);
+	header ("Content-Disposition: attachment; filename=".$desc.".zip");
 	readfile($file);
 ?>
