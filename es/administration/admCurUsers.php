b3537c99eedc2f65acd5dc8f39151a6f518ed5a3
Miguel Ángel Melón Pérez <miguelfito@gmail.com>
2014-03-05 09:26:02 +0100

    Eliminación marcas de conflicto
    Eliminación de las marcas de conflicto para el merge en el
    `admCurUsers.php`

--- es/administration/admCurUsers.php
+++ es/administration/admCurUsers.php
@@ -371,12 +371,7 @@
 												<select name="newULanguage" class="form-control">
 													<option selected disabled value=''>Idioma</option>
 													<?php 
-<<<<<<< HEAD
-														//$siteLanguages = getDBcompletecolumnID('esName', 'siteLanguages', 'id');
-														$siteLanguages = getDBcompletecolumnID(getDBsinglefield('language', 'users', 'login', $_SESSION['loglogin']), 'siteLanguages', 'id');
-=======
 														$siteLanguages = getDBcompletecolumnID('spanish', 'siteLanguages', 'id');
->>>>>>> upstream/development
 														foreach($siteLanguages as $i){
 															echo "<option value=" . $i . ">" . $i . "</option>";
 														}
@@ -468,12 +463,7 @@
 												<select name="newULanguage" class="form-control">
 													<option selected disabled value=''>Idioma</option>
 													<?php 
-<<<<<<< HEAD
-														//$siteLanguages = getDBcompletecolumnID('esName', 'siteLanguages', 'id');
-														$siteLanguages = getDBcompletecolumnID(getDBsinglefield('language', 'users', 'login', $_SESSION['loglogin']), 'siteLanguages', 'id');
-=======
 														$siteLanguages = getDBcompletecolumnID('spanish', 'siteLanguages', 'id');
->>>>>>> upstream/development
 														foreach($siteLanguages as $i){
 															echo "<option value=" . $i . ">" . $i . "</option>";
 														}
