6240e6cacfbaf16e962908e3f52713c100d39a49
Miguel Ángel Melón Pérez <miguelfito@gmail.com>
2014-03-11 00:03:59 +0100

    Modificación de estilo en etiqueta de campo Postal
    Modificación de estilo en etiqueta de campo Postal (`getPostalData.php`)

--- es/getPostalData.php
+++ es/getPostalData.php
@@ -29,7 +29,7 @@ else {
 	$value = intval($_GET['value']);
 	$result = getDBcolumnvalue('cityName', 'postalCitiesES', 'postalCode', $value);
 	if(count($result) > 1){
-		echo '<label id="uploadFormLabel" class="control-label col-sm-2" for="blankaddrcity">Localidad: </label><select class="form-control" name="blankaddrcity" id="blankaddrcity" style="margin-top:5px; width:60%">';
+		echo '<label id="uploadFormLabel" class="control-label col-sm-2" for="blankaddrcity" style="padding-right: 10px;">Localidad: </label><select class="form-control" name="blankaddrcity" id="blankaddrcity" style="margin-top:5px; width:60%">';
 			echo'<option>Su localidad...</option>';
 			foreach($result as $j){
 				$auxi = $j;
@@ -39,10 +39,10 @@ else {
 		echo '</select><br>';
 	}
 	else{
-		echo '<label id="uploadFormLabel" class="control-label col-sm-2" for="blankaddrcity">Localidad: </label><input class="form-control" type="text" name="blankaddrcity" size="50" value="' . $result[0] . '" disabled style="margin-top:5px;"><br>';
+		echo '<label id="uploadFormLabel" class="control-label col-sm-2" for="blankaddrcity" style="padding-right: 10px;">Localidad: </label><input class="form-control" type="text" name="blankaddrcity" size="50" value="' . $result[0] . '" disabled style="margin-top:5px;"><br>';
 	}
-	echo '<label id="uploadFormLabel" class="control-label col-sm-2" for="blankaddrprovince">Provincia: </label><input class="form-control" type="text" name="blankaddrprovince" size="20" value="' . getDBsinglefield('provinceName', 'postalProvincesES', 'id', getDBsinglefield('provCod', 'postalCitiesES', 'postalCode', $value)) . '" disabled style="margin-top:5px;"><br>';
-	echo '<label id="uploadFormLabel" class="control-label col-sm-2" for="blankaddrcountry">País: </label><input class="form-control" type="text" name="blankaddrcountry" size="20" value="España" disabled style="margin-top:5px;"><br>';
+	echo '<label id="uploadFormLabel" class="control-label col-sm-2" for="blankaddrprovince" style="padding-right: 10px;">Provincia: </label><input class="form-control" type="text" name="blankaddrprovince" size="20" value="' . getDBsinglefield('provinceName', 'postalProvincesES', 'id', getDBsinglefield('provCod', 'postalCitiesES', 'postalCode', $value)) . '" disabled style="margin-top:5px;"><br>';
+	echo '<label id="uploadFormLabel" class="control-label col-sm-2" for="blankaddrcountry" style="padding-right: 10px;">País: </label><input class="form-control" type="text" name="blankaddrcountry" size="20" value="España" disabled style="margin-top:5px;"><br>';
 	
 }
 
